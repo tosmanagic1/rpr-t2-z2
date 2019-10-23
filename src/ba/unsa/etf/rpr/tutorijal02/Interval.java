729d724ef45870d349f9e30b8a16033756e9a4b6
Tarik Osmanagic <tarik.osmanagic@gmail.com>
2019-10-23 16:41:19 +0200

    Napisani konstruktori i funkcije isNull i isIn

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,5 +2,39 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
+    private double pocetnaTacka, krajnjaTacka;
+    private boolean pocetnaNaIntervalu, krajnjaNaIntervalu;
 
+    public Interval (double pocetnaTacka, double krajnjaTacka, boolean pocetnaNaIntervalu, boolean krajnjaNaIntervalu) {
+        if ( pocetnaTacka > krajnjaTacka) throw new IllegalArgumentException ("Pocetna tacka veca od krajnje tacke.");
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pocetnaNaIntervalu = pocetnaNaIntervalu;
+        this.krajnjaNaIntervalu = krajnjaNaIntervalu;
+    }
+
+    public Interval () {
+        pocetnaTacka = 0;
+        krajnjaTacka = 0;
+        krajnjaNaIntervalu = null;
+        pocetnaNaIntervalu = null;
+    }
+
+    public boolean isNull () {
+        if ( pocetnaTacka == 0 && krajnjaTacka == 0 ) return true;
+        return false;
+    }
+
+    public boolean isIn (double tacka) {
+
+        if (tacka >= pocetnaTacka && tacka <= krajnjaTacka) return true;
+        return false;
+
+    }
+    public Interval intersect (Interval interval) {
+
+    }
+    public static Interval intersect (Interval interval1, Interval interval2) {
+
+    }
 }
